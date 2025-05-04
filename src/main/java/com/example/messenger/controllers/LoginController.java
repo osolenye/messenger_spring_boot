9d51d83cb9949033a15adeade08ffdf06ba783f0
osolenye <raiimkulov.bekzat@gmail.com>
2025-05-04 22:53:46 +0600

    auth with custom controller

---
+++ src/main/java/com/example/messenger/controllers/LoginController.java
@@ -0,0 +1,30 @@
+package com.example.messenger.controllers;
+
+import lombok.Data;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+@Data
+public class LoginController {
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("")
+    public ResponseEntity<Void> login(@RequestBody LoginRequest loginRequest) {
+        Authentication authenticationRequest =
+                UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.username(), loginRequest.password());
+        Authentication authenticationResponse =
+                this.authenticationManager.authenticate(authenticationRequest);
+        // ...
+        return ResponseEntity.ok().build();
+    }
+    public record LoginRequest(String username, String password) {
+    }
+}
